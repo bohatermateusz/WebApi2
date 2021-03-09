77a4d1fe90e2f7169a8f570f8ff65a51c606ee01
bohatermateusz <bohatermateusz@gmail.com>
2021-03-09 14:02:15 +0100

    added some config

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -13,7 +13,16 @@ namespace WebApplication1
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder()
+              .UseKestrel()
+              .UseContentRoot(Directory.GetCurrentDirectory())
+              .UseConfiguration(config)
+              .UseIISIntegration()
+              .UseStartup<Startup>()
+              .Build();
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
